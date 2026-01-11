79f153c33a460841c968f5f43c69bb7d4036f68c
kush patel <kushpatel3535@gmail.com>
2026-01-11 20:05:42 +0530

    Initial commit — Todo app code

---
+++ src/MyComponents/About.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      This is an about component
+      <p>
+        This Todo App is a simple task management application built using React.
+        It allows users to add, update, delete, and mark tasks as completed. The
+        main goal of this app is to help users organize their daily work and
+        improve productivity. The application uses React components and state
+        management to update the UI dynamically without reloading the page. It
+        provides a clean and user-friendly interface, making task tracking easy
+        and efficient.
+      </p>
+    </div>
+  );
+};
